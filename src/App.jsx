a815099ca7d2e3aaf882012199f3265dde341fc5
donesaShalaa <161884302+donesaShalaa@users.noreply.github.com>
2024-03-03 05:23:24 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React, { createContext, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createContext, useEffect, useState } from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import TVShowList from './components/TVShowList';
+import Popup from './components/Popup'; // Import the Popup component
 
 const MoviesContext = createContext();
 export { MoviesContext };
@@ -10,21 +11,43 @@ export { MoviesContext };
 function App() {
     const [movies, setMovies] = useState([]);
     const [tvShows, setTVShows] = useState([]);
-    const [fetchUrl, setFetchUrl] = useState(
+    const [movieFetchUrl, setMovieFetchUrl] = useState(
         `${import.meta.env.VITE_API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`
     );
-    const [search, setSearch] = useState('');
+    const [tvShowFetchUrl, setTVShowFetchUrl] = useState(
+        `${import.meta.env.VITE_API_BASE_URL}/tv/popular?api_key=${import.meta.env.VITE_API_KEY}`
+    );
+    const [movieSearch, setMovieSearch] = useState('');
+    const [tvShowSearch, setTVShowSearch] = useState('');
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [openPopup, setOpenPopup] = useState(false);
 
     useEffect(() => {
         function fetchMovies() {
-            fetch(fetchUrl)
+            fetch(`${import.meta.env.VITE_API_BASE_URL}/genre/movie/list?api_key=${import.meta.env.VITE_API_KEY}`)
                 .then((response) => response.json())
-                .then((data) => setMovies(data.results))
-                .catch((error) => console.error(error));
+                .then((genreData) => {
+                    fetch(movieFetchUrl)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            const movieResults = data.results.map((movie) => {
+                                const genres = movie.genre_ids.map((genreId) =>
+                                    genreData.genres.find((genre) => genre.id === genreId)
+                                );
+                                return {
+                                    ...movie,
+                                    genres,
+                                };
+                            });
+                            setMovies(movieResults);
+                        })
+                        .catch((error) => console.error('Error fetching movies:', error));
+                })
+                .catch((error) => console.error('Error fetching genres:', error));
         }
 
         function fetchTVShows() {
-            fetch('https://api.themoviedb.org/3/tv/popular?api_key=cb65f66704c9b22be001707cd648d7f3')
+            fetch(tvShowFetchUrl)
                 .then((response) => response.json())
                 .then((data) => setTVShows(data.results))
                 .catch((error) => console.error('Error fetching TV shows:', error));
@@ -32,63 +55,62 @@ function App() {
 
         fetchMovies();
         fetchTVShows();
-    }, [fetchUrl]);
-
-    const getMovies = (movieType) => {
-        let url;
-
-        switch (movieType) {
-            case 'Popular':
-                url = `${import.meta.env.VITE_API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`;
-                break;
-            case 'Drama':
-                url = `${import.meta.env.VITE_API_BASE_URL}/discover/movie?with_genres=18&sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`;
-                break;
-            case 'Kids':
-                url = `${import.meta.env.VITE_API_BASE_URL}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`;
-                break;
-            case 'Thriller':
-                url = `${import.meta.env.VITE_API_BASE_URL}/discover/movie?with_genres=53&sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`;
-                break;
-        }
-
-        setFetchUrl(url);
-    };
+    }, [movieFetchUrl, tvShowFetchUrl]);
 
     const searchMovies = (query) => {
         const movieUrl = `${import.meta.env.VITE_API_BASE_URL}/search/movie?query=${query}&api_key=${import.meta.env.VITE_API_KEY}`;
-        const tvShowUrl = `${import.meta.env.VITE_API_BASE_URL}/search/tv?query=${query}&api_key=${import.meta.env.VITE_API_KEY}`;
-    
-        setFetchUrl(movieUrl); // Start by searching for movies
-        setSearch('');
-        
-        // Fetch TV shows separately
-        fetch(tvShowUrl)
-            .then((response) => response.json())
-            .then((data) => setTVShows(data.results))
-            .catch((error) => console.error('Error fetching TV shows:', error));
+
+        setMovieFetchUrl(movieUrl);
+        setMovieSearch(query);
     };
-    
-    
+
+    const searchTVShows = (query) => {
+        const tvShowUrl = `${import.meta.env.VITE_API_BASE_URL}/search/tv?query=${query}&api_key=${import.meta.env.VITE_API_KEY}`;
+
+        setTVShowFetchUrl(tvShowUrl);
+        setTVShowSearch(query);
+    };
+
+    const clearSearch = () => {
+        setMovieFetchUrl(
+            `${import.meta.env.VITE_API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${import.meta.env.VITE_API_KEY}`
+        );
+        setTVShowFetchUrl(
+            `${import.meta.env.VITE_API_BASE_URL}/tv/popular?api_key=${import.meta.env.VITE_API_KEY}`
+        );
+        setMovieSearch('');
+        setTVShowSearch('');
+    };
+
+    const handleOpenPopup = (item) => {
+        setSelectedItem(item);
+        setOpenPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setOpenPopup(false);
+    };
 
     return (
         <MoviesContext.Provider
             value={{
                 movies,
                 tvShows,
-                getMovies,
-                search,
                 searchMovies,
-                setSearch,
+                searchTVShows,
+                movieSearch,
+                tvShowSearch,
+                clearSearch,
             }}
         >
             <BrowserRouter>
-                <Header searchMovies={searchMovies} />
+                <Header clearSearch={clearSearch} />
                 <Routes>
                     <Route path="/" element={<MovieList />} />
-                    <Route path="/movies" element={<MovieList />} />
-                    <Route path="/tvshows" element={<TVShowList />} />
+                    <Route path="/movies" element={<MovieList handleOpenPopup={handleOpenPopup} />} />
+                    <Route path="/tvshows" element={<TVShowList handleOpenPopup={handleOpenPopup} />} />
                 </Routes>
+                <Popup open={openPopup} handleClose={handleClosePopup} item={selectedItem} />
             </BrowserRouter>
         </MoviesContext.Provider>
     );
